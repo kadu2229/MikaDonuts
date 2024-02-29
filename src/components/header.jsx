010ee9d3a904d59123426883f1fcfc911372b6e2
edu <kadu2229@yahoo.com.br>
2024-02-29 20:16:23 -0300

    trabalhando no header

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import '../styles/header.css';
+import logo from '../images/mikaDonuts.jpeg'
+
+function Header() {
+  return(
+    <div id='header'>
+      <div className='headerItens'>
+        <img id='logo' src={logo} alt="logo mikaDonuts"/>
+        <div id='links'>
+          <a className='headerLinks'>Sobre nós</a>
+          <a href='https://wa.me/5521991355849?text=ol%C3%A1+gostaria+de+fazer+um+or%C3%A7amento' className='headerLinks'>Fale conosco</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
